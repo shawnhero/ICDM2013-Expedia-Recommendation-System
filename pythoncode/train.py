dd961708bed4ab658e70ccaa26a1ae8578d81dcd
Ubuntu <ubuntu@ip-172-31-14-192.us-west-1.compute.internal>
2014-06-12 00:46:30 +0000

    path_modify

--- pythoncode/train.py
+++ pythoncode/train.py
@@ -53,6 +53,8 @@ def feature_eng(train):
 
 def main(sample_size = int(sys.argv[1])):
     train = data_io.read_train()
+    print("Data Size:")
+    print(train.shape)
     feature_eng(train)
     ## originally sample size = 100000
     train_sample = train[:sample_size]
